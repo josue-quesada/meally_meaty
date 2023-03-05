d184d8ba9e079993162419f1d9e857e8637418c5
mariartve <110705321+mariartve@users.noreply.github.com>
2023-03-05 01:08:59 -0600

    búsqueda por parámetros

--- meals_app/src/main.jsx
+++ meals_app/src/main.jsx
@@ -2,6 +2,5 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./index.css";
-import { MealContextProvider } from "./context/MealContext";
 
 ReactDOM.createRoot(document.getElementById("root")).render(<App />);
