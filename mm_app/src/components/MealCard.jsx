05e727cffe3b5123ae72c0d92fd4c44d7d2e52ad
Josué Quesada <josuequesadaxd@gmail.com>
2023-03-14 12:47:12 -0600

    updated ui

--- mm_app/src/components/MealCard.jsx
+++ mm_app/src/components/MealCard.jsx
@@ -13,12 +13,12 @@ const MealCard = ({ meal }) => {
         <div className="cardContainer">
             <h2 className="mealName">{meal.strMeal}</h2>
             <div className="imageContainer">
-                <img className="meal-card-img" src={meal.strMealThumb} alt={meal.strMeal} onClick={handleClick} />
+                <img className="meal-card-img" src={meal.strMealThumb} alt={meal.strMeal} onClick={handleClick}/>
             </div>
             {showDetails && <MealDetails mealId={meal.idMeal} onClose={() => setShowDetails(false)} />}
             <div className="cardContent">
-                <p>{meal.strCategory}</p>
-                <p>{meal.strArea}</p>
+                <p>{meal.strCategory} - {meal.strArea}</p>
+                
             </div>
         </div>
     );
